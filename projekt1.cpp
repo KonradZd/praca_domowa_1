68c90337fc33aeed7285296a2e13eb89d3bfd1af
Konrad Zdziarski <zdziarski@mat.umk.pl>
2024-12-27 13:59:31 +0100

    Dodano uzycie funkcji silnia w pliku projekt1.cpp

--- projekt1.cpp
+++ projekt1.cpp
@@ -20,7 +20,11 @@ int main() {
 	cout << "MENU" << endl;
 	cout << "Podaj numer czynnosci, ktora chcesz wkonac" << endl;
 	cout << "0. Wyjscie" << endl;
+	cout << "1. Silnia" << endl; 
 	cin >> wyjscie;
+	if(wyjscie == 1){
+	   cout << "Silnia z " << a << ": " << silnia(a) << endl;
+	}
     } while(wyjscie != 0);
     return 0;
 }
